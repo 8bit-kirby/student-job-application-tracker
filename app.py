21ee980623961285bd9162913268825f7b8664f6
conejito <hdzdom11@gmail.com>
2026-07-21 16:43:03 -0700

    Add initial Python application

---
+++ app.py
@@ -0,0 +1,6 @@
+company = "NVIDIA"
+position = "Software Engineering Intern"
+status = "Applied"
+
+print(f"I applied to {company} for the position of {position} "
+      f"Application status: {status}.")

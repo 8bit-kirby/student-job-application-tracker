8b496ee103dff605d6ede95c6a1a34e63d57d1e0
conejito <hdzdom11@gmail.com>
2026-07-21 17:10:48 -0700

    Add user input for applications

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-company = "NVIDIA"
-position = "Software Engineering Intern"
-status = "Applied"
+company = input("Enter the company name: ")
+position = input("Enter the position you applied for: ")
+status = input("Enter the application status (e.g., pending, accepted, rejected): ")
 
-print(f"I applied to {company} for the position of {position} "
-      f"Application status: {status}.")
+print(
+    f"I applied to {company} for the position of {position}. "
+      f"Application status: {status}."
+)

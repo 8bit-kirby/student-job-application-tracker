0e13c6dbffc8ef2f67453cdc3a250e46ecf55815
conejito <hdzdom11@gmail.com>
2026-08-19 22:35:24 -0700

    Store application data in dictionary

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
-company = input("Enter the company name: ")
-position = input("Enter the position you applied for: ")
-status = input("Enter the application status (e.g., pending, accepted, rejected): ")
+company = input("Enter the company name: ").strip()
+position = input("Enter the position you applied for: ").strip()
+status = input("Enter the application status (e.g., pending, accepted, rejected): ").strip
+
+application = {
+    "company": company,
+    "position": position,
+    "status": status
+}
 
 print(
-    f"I applied to {company} for the position of {position}. "
-      f"Application status: {status}."
+    f"I applied to {application['company']} for the position of {application['position']}. "
+    f"Application status: {application['status']}."
 )
